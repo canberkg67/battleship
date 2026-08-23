8d2ec5003db7a5077f403f7b51c26c84b3dfe3a6
Canberk <canberkg98@hotmail.com>
2026-08-23 22:07:37 +0300

    feat: add hit implementation to Ship

--- src/Ship.js
+++ src/Ship.js
@@ -1,6 +1,10 @@
 export class Ship {
     constructor(length) {
         this.length = length;
-        
+        this.hits = 0;
+    }
+
+    hit() {
+        this.hits++;
     }
 }
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -5,4 +5,10 @@ describe('Ship', () => {
         const ship = new Ship(3);
         expect(ship.length).toBe(3);
     });
+    test("Hit increase the hit count", () => {
+        const ship = new Ship(3);
+        ship.hit();
+        ship.hit();
+        expect(ship.hits).toBe(2);
+    });
 });
