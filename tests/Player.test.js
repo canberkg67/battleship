d805da141a3ec18a8bcd2480a84ed77b241ac781
Canberk <canberkg98@hotmail.com>
2026-08-25 00:26:19 +0300

    test: add test for hitting enemy gameboard

--- tests/Player.test.js
+++ tests/Player.test.js
@@ -1,4 +1,5 @@
 import { Player } from '../src/Player.js';
+import { Ship } from '../src/Ship.js';
 
 describe('Player', () => {
     test("Player can be created", () => {
@@ -15,5 +16,14 @@ describe('Player', () => {
 
         player.attack(enemy.gameboard, [0, 0]);
         expect(enemy.gameboard.missedAttacks).toContainEqual([0, 0]);
+    });
+    test("Player can attack enemy gameboard and hit", () => {
+        const player = new Player();
+        const enemy = new Player();
+        const ship = new Ship(2);
+        enemy.gameboard.placeShip(ship, [0, 0], 'horizontal');
+
+        player.attack(enemy.gameboard, [0, 0]);
+        expect(ship.hits).toBe(1);
     });
 });
