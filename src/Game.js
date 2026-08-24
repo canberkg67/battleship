416731a3c49afac5d91ca83448c46045f7453215
Canberk <canberkg98@hotmail.com>
2026-08-25 02:25:14 +0300

    feat: add two players in Game Class

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import { Player } from '../src/Player.js';
 
 export class Game {
     constructor() {
-        
+        this.player1 = new Player();
+        this.player2 = new Player();
     }
 }
--- tests/Game.test.js
+++ tests/Game.test.js
@@ -5,4 +5,9 @@ describe('Game', () => {
         const game = new Game();
         expect(game).toBeDefined();
     });
+    test("Game has two players", () => {
+        const game = new Game();
+        expect(game.player1).toBeDefined();
+        expect(game.player2).toBeDefined();
+    });
 });
