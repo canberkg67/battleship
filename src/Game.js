906ae2e269f19338609800ccf1ed70d4e455b94b
Canberk <canberkg98@hotmail.com>
2026-08-28 21:00:26 +0300

    feat: add playTurn method to Game

--- src/Game.js
+++ src/Game.js
@@ -12,6 +12,15 @@ export class Game {
         // If currentPlayer is player1, switch to player2; otherwise, switch to player1
     }
 
+    playTurn(coordinates) {
+        const enemy = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+        const hit = this.currentPlayer.attack(enemy.gameboard, coordinates);
+
+        if (!hit) {
+            this.switchPlayer();
+        }
+    }
+
     isGameOver() {
         return this.player1.gameboard.allShipsSunk() || this.player2.gameboard.allShipsSunk();
     }
