21c9817a469b67fac32a99df12a9f4281254482d
Canberk <canberkg98@hotmail.com>
2026-08-30 23:55:00 +0300

    feat: add name prop in Ship and related tests

--- src/Ship.js
+++ src/Ship.js
@@ -1,6 +1,7 @@
 export class Ship {
-    constructor(length) {
+    constructor(length , name) {
         this.length = length;
+        this.name = name;
         this.hits = 0;
     }
 
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -10,13 +10,13 @@ describe('Ship', () => {
         expect(ship.name).toBe("Submarine");
     });
     test("Hit increase the hit count", () => {
-        const ship = new Ship(3);
+        const ship = new Ship(3 , "Submarine");
         ship.hit();
         ship.hit();
         expect(ship.hits).toBe(2);
     });
     test("Ship is sunk when hits equal length", () => {
-        const ship = new Ship(3);
+        const ship = new Ship(3 , "Submarine");
         ship.hit();
         ship.hit();
         ship.hit();
