dbdc3b2561844c62b81ce47e9d28b82a4b03d216
Canberk <canberkg98@hotmail.com>
2026-09-04 23:40:30 +0300

    feat: create AI class and its chooseAttack method

--- src/AI.js
+++ src/AI.js
@@ -1,1 +1,28 @@
 //This will control attacks of AI behaviour
+
+export class AI {
+    constructor() {
+        this.attackedCoordinates = [];
+    }
+
+    chooseAttack() {
+        let coordinates;
+
+        do {
+            coordinates = [
+                Math.floor(Math.random() * 10),
+                Math.floor(Math.random() * 10)
+            ];
+        } while (
+            this.attackedCoordinates.some(
+                ([row, col]) =>
+                    row === coordinates[0] &&
+                    col === coordinates[1]
+            )
+        );
+
+        this.attackedCoordinates.push(coordinates);
+
+        return coordinates;
+    }
+}
